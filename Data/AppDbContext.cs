658fab31b92012b32739ee604084eb3b3e6767f4
Diego Ramirez <diegoramirez1949@gmail.com>
2025-04-23 22:37:42 -0500

    Agregar archivos de proyecto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CrudApi.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
